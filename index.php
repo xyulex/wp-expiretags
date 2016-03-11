2a52ff7132f7f6089d3da429f4daa28edaefdd1e
xyulex <raulmartinez911@hotmail.com>
2016-03-11 13:20:10 +0100

    General refactor

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 Plugin Name: Expire Tags
 Description: Expire tags allows you to add a date to a tag to expire it.  When the date is reached the tag is no longer associated with the post, but the tag is not removed and the post is not deleted. 
 This could be used to display a custom query by tag of important issues or upcoming events.
-Version: 0.3
+Version: 1.0
 Author:      xyulex
 Author URI:  https://profiles.wordpress.org/xyulex/
 License:     GPLv2 or later
@@ -16,9 +16,7 @@ register_activation_hook( __FILE__, 'expiretags_install' );
 
 // Actions
 add_action( 'admin_menu', 'expiretags_menu' );
-add_action( 'init', 'expiretags_save' );
-add_action( 'init', 'expiretags_expire' );
-add_action( 'init', 'expiretags_translation' );
+add_action( 'init', 'expiretags_init' );
 
 // Cronable
 if ( ! wp_next_scheduled( 'expiretags_checkexpirations' ) ) {
@@ -27,30 +25,34 @@ if ( ! wp_next_scheduled( 'expiretags_checkexpirations' ) ) {
 
 add_action( 'expiretags_check', 'expiretags_expire' );
 
-function expiretags_translation()
-{
+function expiretags_init() {
+    wp_register_script( 'expirejs', plugins_url().'/js/functions.js' );
+    wp_localize_script( 'expirejs', 'URLS', array( 'site_url' => get_option('siteurl'), 'plugins_url' => plugins_url().'/expire-tags') );
+    wp_enqueue_script( 'expirejs' );
+    expiretags_save();
+    expiretags_expire();
     load_plugin_textdomain('expiretags', FALSE, dirname(plugin_basename(__FILE__)).'/languages/');
 }
 
 function expiretags_menu() {
-    add_options_page( 'Options', 'Expire tags', 'manage_options', 'expiretags_options', 'expiretags_options' );
+    add_options_page( 'Options', __('Expire tags'), 'manage_options', 'expiretags_options', 'expiretags_options' );
 }
 
 
-function expiretags_install() {    
+function expiretags_install() {
     require_once( '../wp-admin/includes/upgrade.php' );
     global $wpdb;
     global $charset_collate;
 
     $table_name = $wpdb->prefix . 'expiretags';
-       
+
     $sql = "CREATE TABLE $table_name (
         id int(11) unsigned NOT NULL AUTO_INCREMENT,
         term_id int(11) unsigned NOT NULL,
         expiration_datetime date NOT NULL DEFAULT '0000-00-00',
         UNIQUE KEY id (id)
-        ) $charset_collate;";    
-    
+        ) $charset_collate;";
+
     dbDelta( $sql );
 }
 
@@ -58,9 +60,8 @@ function expiretags_options() {
     global $wpdb;
     wp_enqueue_script( 'jquery-ui-datepicker' );
     wp_enqueue_style( 'expiretags-styles', plugins_url( '/css/style.css', __FILE__ ) );
-    wp_enqueue_script( 'expiretags-scripts', plugins_url('/js/functions.js', __FILE__));    
+    wp_enqueue_script( 'expiretags-scripts', plugins_url('/js/functions.js', __FILE__));
 
-   
     if ( !current_user_can( 'manage_options' ) )  {
         wp_die( __('nopermissions','expiretags') );
     }
@@ -123,31 +124,31 @@ function expiretags_options() {
 }
 
 function expiretags_expire() {
-    global $wpdb; 
-    
+    global $wpdb;
+
     $date = date("Y-m-d");
     $tags = $wpdb->get_results("SELECT * from ". $wpdb->prefix."expiretags where expiration_datetime <'". $date ."'");
 
-    if ($tags){  
-        foreach($tags as $tag) {           
+    if ($tags){
+        foreach($tags as $tag) {
             $tt = $wpdb->get_results("SELECT term_taxonomy_id from ". $wpdb->prefix."term_taxonomy where term_id =" .$tag->term_id);
-            if ($tt) {                
+            if ($tt) {
                 $trs = $wpdb->get_results("select * from ". $wpdb->prefix."term_relationships where term_taxonomy_id=".$tt[0]->term_taxonomy_id);
-                foreach($trs as $tr) {                    
-                    $wpdb->delete( $wpdb->prefix."term_relationships", array( 'object_id' => $tr->object_id ) );                    
+                foreach($trs as $tr) {
+                    $wpdb->delete( $wpdb->prefix."term_relationships", array( 'object_id' => $tr->object_id ) );
                 }
 
-            }  
+            }
             $wpdb->delete( $wpdb->prefix."expiretags", array( 'term_id' => $tag->term_id ) );
         }
-    } 
+    }
 }
 
 function expiretags_save() {
-    global $wpdb;    
-    
+    global $wpdb;
+
     $table = $wpdb->prefix."expiretags";
-    
+
     if ($_POST) {
         // Save data
         foreach($_POST as $key=>$value) {
@@ -155,22 +156,22 @@ function expiretags_save() {
                 $data = array(
                         'term_id'               => $key,
                         'expiration_datetime'   => $value
-                        );                           
+                        );
 
                 $exists = $wpdb->get_results (
                         "
-                        SELECT * 
-                        FROM  ". $wpdb->prefix."expiretags 
+                        SELECT *
+                        FROM  ". $wpdb->prefix."expiretags
                         WHERE term_id = " . $key
                         );
-                if (!$exists) {                   
+                if (!$exists) {
                     $wpdb->insert($table, $data);
-                } else {                    
+                } else {
                     $where = array ('term_id' => $key);
                     $wpdb->update($table, $data, $where);
                 }
-            }            
-        }        
+            }
+        }
     }
 }
 ?>
